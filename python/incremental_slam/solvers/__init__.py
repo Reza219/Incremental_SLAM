fba23f33f9622849cd69741efba58a249a0f5baf
Reza Arablouei <rezaa219@yahoo.com>
2026-05-07 15:06:41 +1000

    Create __init__.py

---
+++ python/incremental_slam/solvers/__init__.py
@@ -0,0 +1,3 @@
+from .full import solve_full, converged
+from .selective import solve_affected, SelectiveSolveResult
+from .cholmod_manager import CholmodNormalManager, EdgeContribution, cholmod_available
